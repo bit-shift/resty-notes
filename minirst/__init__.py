3b98eac1fb15f617cfb8adf7eac881734c4e9d90
Bit Shift <bitshift@bigmacintosh.net>
2012-11-05 08:53:50 +0000

    Cleaner, shorter, less hacky to_dict() implementation.

--- minirst/__init__.py
+++ minirst/__init__.py
@@ -14,20 +14,13 @@ def to_dict(r):
 
 Parse a minimal reST-like document, r, into a dict of title:block."""
     blocks = []
-
-    last_line = ""
     in_lines = r.split("\n")
-    in_lines.append("")  # pad to avoid special-casing final line
-    for line in in_lines:
-        if (len(line) > 0) and (len(line) == len(last_line)) and (line.count("=") == len(last_line)):
-            if len(blocks) > 0: # last block would have consumed the title, remove it
-                blocks[-1] = [blocks[-1][0], "\n".join(blocks[-1][1].split("\n")[:-1])]
-            blocks.append([last_line, ""])
+    for n in xrange(len(in_lines)):
+        if n < (len(in_lines) - 1) and len(in_lines[n]) == len(in_lines[n+1]) and len(in_lines[n]) == in_lines[n+1].count("="):
+            blocks.append([in_lines[n], ""])
+        elif n < len(in_lines) and len(in_lines[n-1]) == len(in_lines[n]) and len(in_lines[n-1]) == in_lines[n].count("="):
+            continue
         elif len(blocks) > 0:
-            if blocks[-1][1] != "":
-                blocks[-1][1] = blocks[-1][1] + "\n" + line
-            else:
-                blocks[-1][1] = line
-        last_line = line
+            blocks[-1][1] += in_lines[n] + "\n"
 
     return dict(blocks)
